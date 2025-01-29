9ce489bef4cdcd9197c1a50a7bcba200884fd240
maksimangeou <ange_fuck@icloud.com>
2025-01-29 16:30:28 +0300

    initial commit

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,2 @@
 public class Main {
-    public static void main(String[] args) {
-        //task1
-        StringBuilder strConcatenation = new StringBuilder();
-        String firstName = "Ivan";
-        String middleName = "Ivanovich";
-        String lastName = "Ivanov";
-        String fullName = strConcatenation.append(lastName).append(" ").append(firstName).append(" ").append(middleName).toString();
-        System.out.println("Ф.И.О. сотрудника - " + fullName);
-
-        //task2
-        fullName = fullName.toUpperCase();
-        System.out.println("Данные Ф. И. О. сотрудника для заполнения отчета — " + fullName);
-
-        //task3
-        fullName = "Иванов Семён Семёнович";
-        String fullNameUpper = fullName.replace('ё', 'е');
-        System.out.println("Данные Ф. И. О. сотрудника — " + fullNameUpper);
-    }
 }
